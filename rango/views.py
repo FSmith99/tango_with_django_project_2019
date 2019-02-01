deb8a81a24711e8fe2555682f645ba6130d9bc62
Frazer Smith <2324003s@student.gla.ac.uk>
2019-02-01 14:58:08 +0000

    Chapter 8 End

--- rango/views.py
+++ rango/views.py
@@ -81,5 +81,8 @@ def index(request):
         return render(request,'rango/index.html', context_dict)
 
 def about(request):
-    return render(request,'rango/about.html')
+
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 	
