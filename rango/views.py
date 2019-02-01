79e8516365be03798b5b366fe00288756c0c61a7
Frazer Smith <2324003s@student.gla.ac.uk>
2019-02-01 15:50:42 +0000

    Chapter 9.6

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
 
 def add_category(request):
         form = CategoryForm()
@@ -85,4 +86,37 @@ def about(request):
     print(request.method)
     print(request.user)
     return render(request, 'rango/about.html', {})
+
+def register(request):
+        registered = False
+
+        if request.method == 'POST':
+                user_form = UserForm(data=request.POST)
+                profile_form = UserProfileForm(data=request.POST)
+
+                if user_form.is_valid() and profile_form.is_valid():
+                        user = user_form.save()
+                        user.set_password(user.password)
+                        user.save()
+
+                        profile = profile_form.save(commit=False)
+                        profile.user = user
+
+                        if 'picture' in request.FILES:
+                                profile.picture = request.FILES['picture']
+
+                        profile.save()
+
+                        registered = True
+                else:
+                        print(user_form.errors, profile_form.errors)
+        else:
+                user_form = UserForm()
+                profile_form = UserProfileForm()
+
+        return render(request,
+                      'rango/register.html',
+                      {'user_form': user_form,
+                       'profile_form': profile_form,
+                       'registered': registered})
 	
